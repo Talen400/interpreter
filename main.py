80c0bdf0366a9d18369d0a20a1d905be00789c55
Talen <talengithub0@gmail.com>
2024-08-29 23:02:27 -0400

    add lex()

--- main.py
+++ main.py
@@ -1,34 +1,42 @@
-import sys
 
-def lex(token):
-    i = 0
-    while i < len(token):
-        arg = token[i]["list"].split()
-        pointer = 0
-        while pointer < len(arg):
-            
-            pointer += 1
-        i += 1
-            
 
-    return token    
+def is_digit(arg):
+    return (arg >= '0' and arg <= '9')
 
-def lines(file):
-    lines = []
-    value = 0
-    i = 0
-    while i < len(file):
-        lines.append({"type": "line", "value": value, "list": file[i]})
-        i += 1
-        value += 1
-        pass
-    return lines
+def is_name(arg):
+     return(arg >= 'a' or arg <= 'z' or
+            arg >= 'A' or arg <= 'Z')
+
+def lex(input):
+    input = input.split()
+    pointer = 0
+    out = []
+    while pointer < len(input):
+        arg = input[pointer]
+        
+        if arg == '=':
+             out.append({"type": "operator", "value": arg})
+        elif arg == ';':
+              out.append({"type": "end", "value": arg})
+        elif is_digit(arg):
+              out.append({"type": "number", "value": arg})
+        elif is_name(arg):
+              out.append({"type": "number", "value": arg})
+
+        pointer += 1
+    return out
+
+
+
+def read_file(path):
+    file = open(path, "r")
+    input = file.read()
+    return input
 
 def main():
-    program_file_path = sys.argv[1]
-    program = []
-    with open(program_file_path, 'r') as program_file:
-        program = lines([line for line in program_file])
-    lex(program)
-    print(program)
+    input = read_file("program1.lisp")
+    tokens = lex(input)
+    print(tokens)
+    
+
 main()
